5924343c617b723dc7c37c34b9980e01fe2e4230
fclaerho <contact@fclaerhout.fr>
2015-08-05 18:01:10 +0200

    simplify http_request tracing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
 	name = "pyrest",
 	author = "fclaerhout.fr",
 	license = "MIT",
-	version = "1.2.3",
+	version = "1.3.0",
 	py_modules = ["rest"],
 	test_suite = "test",
 	author_email = "contact@fclaerhout.fr",
